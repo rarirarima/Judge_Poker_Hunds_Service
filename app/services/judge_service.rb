3b0947bdb2ec04473ffd086804c22d37214313ab
rarirarima <ryosuke.2236@icloud.com>
2022-03-30 19:56:31 +0900

    変数名の変更

--- app/services/judge_service.rb
+++ app/services/judge_service.rb
@@ -1,9 +1,9 @@
 module JudgeService
   include ErrorService
-  
+
   def search_hands(cards)
     @suits = cards.scan(/[CDHS]/) # @suits=["H", "H", "H", "H", "H"]
-    @nums_int = cards.scan(/1[0-3]|[1-9]/).map(&:to_i) # @nums_int=[3, 4, 5, 6, 7]
+    @nums = cards.scan(/1[0-3]|[1-9]/).map(&:to_i) # @nums=[3, 4, 5, 6, 7]
     decide_hands
   end
 
@@ -13,7 +13,7 @@ module JudgeService
 
   def decide_hands
     if straight_flush?
-      { name: 'ストレートフラッシュ', strength: 8 } # {役, 強さを表す数字}
+      { name: 'ストレートフラッシュ', strength: 8 } # {役, 強さを表す数値}
     elsif number_card?(4)
       { name: 'フォー・オブ・ア・カインド', strength: 7 }
     elsif fullhouse?
@@ -50,9 +50,9 @@ module JudgeService
   end
 
   def straight?
-    if (@nums_int.sort[4] - @nums_int.min) == 4 && all_unique?
+    if (@nums.sort[4] - @nums.min) == 4 && nums_all_unique?
       true
-    elsif royal_straight? && all_unique?
+    elsif royal_straight? && nums_all_unique?
       true
     else
       false
@@ -73,11 +73,11 @@ module JudgeService
 
   def royal_straight?
     royal_straight_array = [1, 10, 11, 12, 13]
-    return true if royal_straight_array.all? { |i| @nums_int.include?(i) }
+    return true if royal_straight_array.all? { |i| @nums.include?(i) }
   end
 
-  def all_unique?
-    true if @nums_int.uniq.count == 5
+  def nums_all_unique?
+    true if @nums.uniq.count == 5
   end
 
   def number_card?(number)
@@ -89,13 +89,13 @@ module JudgeService
 
   def count_card_num
     num_list = Array.new(13, 0)
-    @nums_int.each do |num|
+    @nums.each do |num|
       num_list[num - 1] += 1
     end
     num_list
   end
 
   module_function :search_hands, :decide_hands, :straight_flush?, :four_of_a_kind?, :fullhouse?, :flush?, :straight?,
-                  :three_of_a_kind?, :two_pair?, :one_pair?, :royal_straight?, :all_unique?, :number_card?,
+                  :three_of_a_kind?, :two_pair?, :one_pair?, :royal_straight?, :nums_all_unique?, :number_card?,
                   :count_card_num
 end
