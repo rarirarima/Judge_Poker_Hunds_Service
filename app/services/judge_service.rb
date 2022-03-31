34e7b66ae3c746993572e7eab8fee5cf5bd34d7f
rarirarima <ryosuke.2236@icloud.com>
2022-03-31 18:27:47 +0900

    メソッド名変更

--- app/services/judge_service.rb
+++ app/services/judge_service.rb
@@ -58,7 +58,7 @@ module JudgeService
   end
 
   def two_pair?
-    true if count_card_num.count(2) == 2
+    true if count_same_num_cards.count(2) == 2
   end
 
   def one_pair?
@@ -74,17 +74,17 @@ module JudgeService
     true if @nums.uniq.count == 5
   end
 
-  # 引数の数がcount_card_numの配列内に存在するか確認
-  def number_card?(number)
-    count_card_num.each do |count|
-      return true if count == number
+  # 引数の数がcount_same_num_cardsの配列内に存在するか確認
+  def number_card?(num)
+    count_same_num_cards.each do |count|
+      return true if count == num
     end
     false
   end
 
   # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]を用意
-  # カードの数字が1の場合, 配列の1番目(index==0)の要素を+1
-  def count_card_num
+  # カードの数字が1の場合, 配列の1番目(index==0)の要素を＋1
+  def count_same_num_cards
     num_list = Array.new(13, 0)
     @nums.each do |num|
       num_list[num - 1] += 1
@@ -94,5 +94,5 @@ module JudgeService
 
   module_function :search_hand, :decide_hands, :straight_flush?, :four_of_a_kind?, :fullhouse?, :flush?, :straight?,
                   :three_of_a_kind?, :two_pair?, :one_pair?, :royal_straight?, :nums_all_unique?, :number_card?,
-                  :count_card_num
+                  :count_same_num_cards
 end
