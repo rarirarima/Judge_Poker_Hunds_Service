754b9c6dccfc86b5060842733b5f22a5f9f698f2
rarirarima <ryosuke.2236@icloud.com>
2022-03-31 12:38:10 +0900

    change_to_bestの追加

--- app/services/strength_service.rb
+++ app/services/strength_service.rb
@@ -1,11 +1,15 @@
 module StrengthService
-  def decide_best(strengths, result)
+  def search_best(strengths, result)
     bests = strengths.each_index.select { |i| strengths[i] == strengths.max }
+    change_to_best(bests, result)
+  end
+
+  def change_to_best(bests, result)
     bests.each do |best|
       result[best][:best] = true
     end
     result
   end
 
-  module_function :decide_best
+  module_function :search_best, :change_to_best
 end
